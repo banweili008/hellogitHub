60530348c94a10ddea6dca0192827b88f8a034c2
banweili008 <452513627@qq.com>
2022-11-17 15:33:57 +0800

    分支代码提交

--- src/main/java/com/example/gittest/controller/TestController.java
+++ src/main/java/com/example/gittest/controller/TestController.java
@@ -10,6 +10,7 @@ public class TestController {
     @RequestMapping("/hello")
     public String hello(){
         System.out.print("test");
+        System.out.println("branch commit");
         return "hello gitHub123";
     }
 }
